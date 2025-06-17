a2428911df8af878e6a349c255d552ec2e80b322
H. Kamran <hkamran@hkamran.com>
2025-06-17 13:46:47 -0700

    Move annotations to completions

--- .config/nvim/lua/plugins/annotations.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"danymat/neogen",
-	config = function()
-		local opts = { noremap = true, silent = true }
-		vim.keymap.set("n", "<leader>nf", ":lua require('neogen').generate()<CR>", opts)
-
-		require("neogen").setup({ snippet_engine = "luasnip" })
-	end,
-}
--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -15,6 +15,15 @@ return {
 		config = true,
 	},
 	{
+		"danymat/neogen",
+		config = function()
+			local opts = { noremap = true, silent = true }
+			vim.keymap.set("n", "<leader>nf", ":lua require('neogen').generate()<CR>", opts)
+
+			require("neogen").setup({ snippet_engine = "luasnip" })
+		end,
+	},
+	{
 		"hrsh7th/nvim-cmp",
 		dependencies = { "micangl/cmp-vimtex" },
 		config = function()
