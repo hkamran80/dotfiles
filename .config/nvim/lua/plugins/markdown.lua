227447fa270cf437c45c67edba5ff7a1e825d9aa
H. Kamran <hkamran@hkamran.com>
2025-08-08 01:12:02 -0700

    nvim: add auto-pandoc.nvim

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,27 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-tree/nvim-web-devicons",
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		ft = "markdown",
 	},
-	ft = "markdown",
+	{
+		"jghauser/auto-pandoc.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		ft = "markdown",
+		config = function()
+			vim.api.nvim_create_autocmd("BufEnter", {
+				pattern = "*.md",
+				callback = function()
+					vim.keymap.set("n", "go", function()
+						require("auto-pandoc").run_pandoc()
+					end, { silent = true, buffer = 0 })
+				end,
+				group = vim.api.nvim_create_augroup("setAutoPandocKeymap", {}),
+				desc = "Set keymap for auto-pandoc",
+			})
+		end,
+	},
 }
